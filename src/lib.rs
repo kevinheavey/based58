8db8a7b8f2ebd3974c634905e0555be3daf04013
Kevin Heavey <kevinheavey123@gmail.com>
2022-02-18 02:31:32 +0000

    add default arg

--- src/lib.rs
+++ src/lib.rs
@@ -11,19 +11,33 @@ fn to_py_value_err<T: Error>(err: T) -> PyErr {
 }
 
 #[pyclass]
-#[derive(Debug)]
-pub struct Alphabet(AlphabetOriginal);
+#[derive(Debug, Clone)]
+pub struct Alphabet(pub AlphabetOriginal);
 
 #[pymethods]
 impl Alphabet {
+    /// Bitcoin's alphabet as defined in their Base58Check encoding.
+    ///
+    /// See <https://en.bitcoin.it/wiki/Base58Check_encoding#Base58_symbol_chart>
     #[classattr]
     const BITCOIN: Self = Self(*AlphabetOriginal::BITCOIN);
+    /// Monero's alphabet as defined in this forum post.
+    ///
+    /// See <https://forum.getmonero.org/4/academic-and-technical/221/creating-a-standard-for-physical-coins>
     #[classattr]
     const MONERO: Self = Self(*AlphabetOriginal::MONERO);
+    /// Ripple's alphabet as defined in their wiki.
+    ///
+    /// See <https://wiki.ripple.com/Encodings>
     #[classattr]
     const RIPPLE: Self = Self(*AlphabetOriginal::RIPPLE);
+    /// Flickr's alphabet for creating short urls from photo ids.
+    ///
+    /// See <https://www.flickr.com/groups/api/discuss/72157616713786392/>
     #[classattr]
     const FLICKR: Self = Self(*AlphabetOriginal::FLICKR);
+    /// The default alphabet used if none is given. Currently is the
+    /// [`BITCOIN`](Self::BITCOIN) alphabet.
     #[classattr]
     const DEFAULT: Self = Self(*AlphabetOriginal::DEFAULT);
 
@@ -45,15 +59,18 @@ impl Alphabet {
     }
 }
 
-#[pyfunction]
-pub fn b58decode<'a>(val: &[u8], py: Python<'a>) -> PyResult<&'a PyBytes> {
-    let byte_array = decode(val).into_vec().map_err(to_py_value_err)?;
+#[pyfunction(alphabet = "Alphabet::BITCOIN")]
+pub fn b58decode<'a>(val: &[u8], alphabet: Alphabet, py: Python<'a>) -> PyResult<&'a PyBytes> {
+    let byte_array = decode(val)
+        .with_alphabet(&alphabet.0)
+        .into_vec()
+        .map_err(to_py_value_err)?;
     Ok(PyBytes::new(py, byte_array.as_slice()))
 }
 
-#[pyfunction]
-pub fn b58encode<'a>(val: &[u8], py: Python<'a>) -> &'a PyBytes {
-    let byte_array = encode(val).into_vec();
+#[pyfunction(alphabet = "Alphabet::BITCOIN")]
+pub fn b58encode<'a>(val: &[u8], alphabet: Alphabet, py: Python<'a>) -> &'a PyBytes {
+    let byte_array = encode(val).with_alphabet(&alphabet.0).into_vec();
     PyBytes::new(py, byte_array.as_slice())
 }
 
